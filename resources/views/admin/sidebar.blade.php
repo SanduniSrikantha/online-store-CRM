2b23be51cc166a69197b3df02d4d20b470063913
SanduniSrikantha <CB010303@students.apiit.lk>
2023-06-05 12:22:16 +0530

    update db

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -69,14 +69,14 @@
               <span class="menu-icon">
                 <i class="mdi mdi-laptop"></i>
               </span>
-              <span class="menu-title">Users</span>
+              <span class="menu-title">Products</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Create</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Delete</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Data Management</a></li>
+                <li class="nav-item"> <a class="nav-link" href="">Add Products</a></li>
+                <li class="nav-item"> <a class="nav-link" href="">Show Products</a></li>
+                <li class="nav-item"> <a class="nav-link" href="">Data Management</a></li>
               </ul>
             </div>
           </li>
